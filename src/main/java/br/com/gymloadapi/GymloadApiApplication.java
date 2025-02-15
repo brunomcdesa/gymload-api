bd79f47ddb96e376ee3350c8636ac7a33c1ad0d0
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2025-02-15 16:04:48 -0300

    Initial commit

---
+++ src/main/java/br/com/gymloadapi/GymloadApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gymloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymloadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymloadApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gymloadapi/GymloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gymloadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymloadApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
