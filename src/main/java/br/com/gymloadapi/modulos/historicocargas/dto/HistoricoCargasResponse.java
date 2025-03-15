0d06eec1a83c1e5f73d53dda742a251475aec794
Brunomcdesa <brunotkd68@gmail.com>
2025-03-15 17:19:22 -0300

    Adiciona id no historico de cargas

--- src/main/java/br/com/gymloadapi/modulos/historicocargas/dto/HistoricoCargasResponse.java
+++ src/main/java/br/com/gymloadapi/modulos/historicocargas/dto/HistoricoCargasResponse.java
@@ -3,10 +3,11 @@ package br.com.gymloadapi.modulos.historicocargas.dto;
 import br.com.gymloadapi.modulos.exercicio.enums.ETipoExercicio;
 
 public record HistoricoCargasResponse(
-        String exercicioNome,
-        String carga,
-        ETipoExercicio tipoExercicio,
-        String grupoMuscularNome,
-        Integer qtdRepeticoes
+    Integer id,
+    String exercicioNome,
+    String carga,
+    ETipoExercicio tipoExercicio,
+    String grupoMuscularNome,
+    Integer qtdRepeticoes
 ) {
 }
