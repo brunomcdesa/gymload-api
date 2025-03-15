78a35002ef23a65521854014db759267a6d28ca8
Brunomcdesa <brunotkd68@gmail.com>
2025-03-15 16:56:08 -0300

    Adiciona id na Response

--- src/main/java/br/com/gymloadapi/modulos/exercicio/dto/ExercicioResponse.java
+++ src/main/java/br/com/gymloadapi/modulos/exercicio/dto/ExercicioResponse.java
@@ -4,6 +4,7 @@ import br.com.gymloadapi.modulos.exercicio.enums.ETipoExercicio;
 import br.com.gymloadapi.modulos.exercicio.enums.ETipoPegada;
 
 public record ExercicioResponse(
+    Integer id,
     String nome,
     String descricao,
     String grupoMuscularNome,
